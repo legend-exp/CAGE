25eb2498c9431e575971ab818e4dcfe3526d70d9
wisecg <wisecg.neontetra@gmail.com>
2019-11-28 12:28:44 -0800

    start oppi plots

--- analysis/clint.mpl
+++ analysis/clint.mpl
@@ -5,34 +5,36 @@ figure.figsize   : 8, 5         # figure size in inches
 #figure.dpi       : 200          # figure dots per inch
 figure.facecolor : white
 figure.edgecolor : white
-#figure.autolayout : True        # Automatically apply 'plt.tight_layout'
+figure.autolayout : True        # Automatically apply 'plt.tight_layout'
 
 lines.linewidth : 3
 lines.markersize : 10
-
 legend.fontsize : 15
 legend.handlelength : 1
-
 axes.titlesize : 17
-axes.labelsize : 17
-
+axes.labelsize : 15
+axes.linewidth : 1
 xtick.labelsize : 14 # font size of tick labels
+xtick.direction : in
+ytick.direction : in
 ytick.labelsize : 14
+xtick.major.size : 10      # major tick size in points
+xtick.minor.size : 5     # minor tick size in points
+ytick.major.size : 10
+ytick.minor.size : 5
 
-xtick.major.size     : 6      # major tick size in points
-xtick.minor.size     : 4      # minor tick size in points
-ytick.major.size     : 6
-ytick.minor.size     : 4
+#xtick.major.size : 20
+#xtick.major.width : 4
+#xtick.minor.size : 10
+#xtick.minor.width : 2
 
 xtick.minor.visible  : True   # turn minor ticks on by default
 ytick.minor.visible  : True
 
-# has to be a .ttf file in /Library/Fonts
-# font.family      : sans-serif
-# font.sans-serif : Helvetica Neue
-#font.sans-serif : 'Comic Sans'
-#font.style : italic
-font.serif : Optima
-# font.family : Helvetica Neue Light
-# font.style : light
-font.family : Optima
+# --- I like this but most people don't have it
+#font.family : Optima
+
+# --- this one is still distinct but should be more widely usable
+font.family : Courier New
+font.weight : bold
+axes.labelweight : bold
--- analysis/oppi.py
+++ analysis/oppi.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('clint.mpl')
+from pprint import pprint
+
+from pygama import DataSet
+import pygama.analysis.histograms as ph
 
 def main():
     """
@@ -19,7 +23,120 @@ def main():
     - look at A/E distrbution
     - run calibration.py (separately) and plot the calibration constants
     """
-    print("hi")
+    tier2_spec()
+    tier1_wfs()
+    
+    
+def tier2_spec():
+    """
+    show a few examples of energy spectra (onboard E and offline E)
+    """
+    run = 42
+    ds = DataSet(run=run, md="runDB.json")
+    t2df = ds.get_t2df()
+    # print(t2df.columns)
+    
+    # # onboard E
+    # ene = "energy"
+    # # xlo, xhi, xpb = 0, 20e6, 5000 # show muon peak (full dyn. range)
+    # xlo, xhi, xpb = 0, 2e6, 2000 # show phys. spectrum (top feature is 2615 pk)
+    
+    # # trap_max E
+    # ene = "etrap_max"
+    # xlo, xhi, xpb = 0, 50000, 100 # muon peak
+    # xlo, xhi, xpb = 0, 6000, 10 # gamma spectrum
+    
+    # fixed time pickoff E 
+    ene = "e_ftp"
+    # xlo, xhi, xpb = 0, 50000, 100 # muon peak
+    xlo, xhi, xpb = 0, 6000, 10 # gamma spectrum
+
+    # get histogram
+    hE, xE = ph.get_hist(t2df[ene], range=(xlo, xhi), dx=xpb)
+    
+    # make the plot
+    plt.semilogy(xE, hE, ls='steps', lw=1, c='r', label=f'run {run}')
+    plt.xlabel("Energy (uncal.)", ha='right', x=1)
+    plt.ylabel("Counts", ha='right', y=1)
+    
+    # show a couple formatting tricks
+    import matplotlib.ticker as ticker
+    plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0e'))
+    plt.grid(linestyle=':')
+    
+    plt.legend()
+    # plt.show()
+    plt.savefig(f"./plots/cage_run{run}_{ene}.pdf")
+    
+
+def tier1_wfs():
+    """
+    show some waveforms, with an example of a data cleaning cut.
+    """
+    run = 42
+    ds = DataSet(run=run, md="runDB.json")
+    ft1 = ds.paths[run]["t1_path"]
+    t1df = pd.read_hdf(ft1, "ORSIS3302DecoderForEnergy", where="ievt < 10000")
+    t1df.reset_index(inplace=True) # required step -- fix pygama "append" bug
+    
+    # for convenience, separate data columns from waveform columns
+    d_cols, wf_cols = [], []
+    for col in t1df.columns:
+        if isinstance(col, int):
+            wf_cols.append(col)
+        else:
+            d_cols.append(col)
+    
+    # get waveform dataframe
+    wfs = t1df[wf_cols]
+    
+    # apply a cut
+    idx_cut = t1df.index[t1df.energy > 10000].tolist()
+    
+    exit()
+
+    
+    # iterate over the waveform block and optionally save some waveform plots
+    iwf = -1
+    while True:
+        if iwf != -1:
+            inp = input()
+            if inp == "q": exit()
+            if inp == "p": iwf -= 2
+        iwf += 1
+        print(iwf)
+
+        wf = wfs.iloc[iwf]
+        ts = np.arange(len(wf))
+
+        plt.cla()
+        
+        # raw waveform
+        plt.plot(ts, wf, "-b", alpha=0.6, label='raw wf')
+        
+        # savitzky-golay smoothed
+        wfsg = signal.savgol_filter(wf, window=47, order=2)
+        plt.plot(ts, wfsg, "-r", label='savitzky-golay filter')
+
+        plt.xlabel("clock ticks", ha='right', x=1)
+        plt.ylabel("ADC", ha='right', y=1)
+        plt.legend()
+        plt.tight_layout()
+        plt.show(block=False)
+        plt.pause(0.01)
+    
+    
+
+def tier2():
+    """
+    """
+    ds = DataSet(3, config="runDB.json")
+    df = ds.get_t2df()
+    print(df.columns)
+    # df.hist(etype)
+    # plt.show()
+    
+    
 
 
 if __name__=="__main__":
