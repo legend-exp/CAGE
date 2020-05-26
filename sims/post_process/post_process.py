793ef77564066e3108b29dccf5d5b95ba08c4528
gothman <gulden.othman@gmail.com>
2020-05-26 09:34:20 -0400

    fixed indentation error

--- sims/post_process/post_process.py
+++ sims/post_process/post_process.py
@@ -32,59 +32,64 @@ def main():
                 '../alpha/processed_out/processed_newDet_sourceRotNorm_y6mm_ICPC_Pb_241Am_100000000.hdf5']
 
     for file in range(len(filenames)):
-        post_process(filenames[file], processed_filename[file])
-
-
+        post_process(filenames[file], processed_filenames[file])
 
 
 def post_process(filename, processed_filename, hits=False):
 	print('Processing file: ', filename)
 	if hits==True:
-		procdf, sourcePV_df = pandarize(filename, source, hits=True)
+		procdf, pos_df = pandarize(filename, hits=True)
 		# df.to_hdf('../alpha/processed_out/processed_newDet_test.hdf5', key='procdf', mode='w')
 		procdf.to_hdf(processed_filename, key='procdf', mode='w')
-		sourcePV_df.to_hdf(processed_filename, key='sourcePV_df', mode='w')
+		pos_df.to_hdf(processed_filename, key='pos_df', mode='w')
 	else:
-		procdf = pandarize(filename, source, hits=False)
+		procdf = pandarize(filename, hits=False)
 		# df.to_hdf('../alpha/processed_out/processed_newDet_test.hdf5', key='procdf', mode='w')
 		procdf.to_hdf(processed_filename, key='procdf', mode='w')
 
 	print('File processed. Output saved to: ', processed_filename)
 
+
 def pandarize(filename, hits=False):
-	# have to open the input file with h5py (g4 doesn't write pandas-ready hdf5)
-	g4sfile = h5py.File(filename, 'r')
-	g4sntuple = g4sfile['default_ntuples']['g4sntuple']
+    # have to open the input file with h5py (g4 doesn't write pandas-ready hdf5)
+    g4sfile = h5py.File(filename, 'r')
+    g4sntuple = g4sfile['default_ntuples']['g4sntuple']
 
-	# build a pandas DataFrame from the hdf5 datasets we will use
-	# list(g4sfile['default_ntuples']['g4sntuple'].keys())=>['Edep','KE','columns','entries','event',
-	# 'forms','iRep','lx','ly','lz','nEvents','names','parentID','pid','step','t','trackID','volID','x','y','z']
+    # build a pandas DataFrame from the hdf5 datasets we will use
+    # list(g4sfile['default_ntuples']['g4sntuple'].keys())=>['Edep','KE','columns','entries','event',
+    # 'forms','iRep','lx','ly','lz','nEvents','names','parentID','pid','step','t','trackID','volID','x','y','z']
 
-	g4sdf = pd.DataFrame(np.array(g4sntuple['event']['pages']), columns=['event'])
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['step']['pages']), columns=['step']),
-	                   lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['Edep']['pages']), columns=['Edep']),
-	                   lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['volID']['pages']),
-	                   columns=['volID']), lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['iRep']['pages']),
-	                   columns=['iRep']), lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['pid']['pages']),
-                       columns=['pid']), lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['x']['pages']),
-                       columns=['x']), lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['y']['pages']),
-                       columns=['y']), lsuffix = '_caller', rsuffix = '_other')
-	g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['z']['pages']),
-                       columns=['z']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = pd.DataFrame(np.array(g4sntuple['event']['pages']), columns=['event'])
 
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['step']['pages']), columns=['step']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['Edep']['pages']), columns=['Edep']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['volID']['pages']), columns=['volID']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['iRep']['pages']), columns=['iRep']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['pid']['pages']), columns=['pid']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['x']['pages']), columns=['x']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['y']['pages']), columns=['y']), lsuffix = '_caller', rsuffix = '_other')
+    g4sdf = g4sdf.join(pd.DataFrame(np.array(g4sntuple['z']['pages']), columns=['z']), lsuffix = '_caller', rsuffix = '_other')
 
-	# print(g4sdf)
-	# xarr = np.array(g4sdf['volID'])
-	# print(xarr[:])
-	# print(type(g4sntuple['x']['pages']))
-	# exit()
+    detector_hits = g4sdf.loc[(g4sdf.Edep>1.e-6)&(g4sdf.volID==1)]
 
+    detector_hits['x_weights'] = detector_hits['x'] * detector_hits['Edep']
+    detector_hits['y_weights'] = detector_hits['y'] * detector_hits['Edep']
+    detector_hits['z_weights'] = detector_hits['z'] * detector_hits['Edep']
+
+    procdf= pd.DataFrame(detector_hits.groupby(['event','volID'], as_index=False)['Edep','x_weights','y_weights', 'z_weights', 'pid'].sum())
+
+    # rename the summed energy depositions for each step within the event to "energy". This is analogous to the event energy you'd see in your detector
+    procdf = procdf.rename(columns={'Edep':'energy'})
+
+    procdf['x'] = procdf['x_weights']/procdf['energy']
+    procdf['y'] = procdf['y_weights']/procdf['energy']
+    procdf['z'] = procdf['z_weights']/procdf['energy']
+
+    del procdf['x_weights']
+    del procdf['y_weights']
+    del procdf['z_weights']
+
+    return procdf
 
 
     if hits==True:
@@ -126,30 +131,11 @@ def pandarize(filename, hits=False):
 
         return pos_df
 
-
-    detector_hits = g4sdf.loc[(g4sdf.Edep>1.e-6)&(g4sdf.volID==1)]
-
-
-
-	detector_hits['x_weights'] = detector_hits['x'] * detector_hits['Edep']
-	detector_hits['y_weights'] = detector_hits['y'] * detector_hits['Edep']
-	detector_hits['z_weights'] = detector_hits['z'] * detector_hits['Edep']
-
-	procdf= pd.DataFrame(detector_hits.groupby(['event','volID'], as_index=False)['Edep','x_weights','y_weights', 'z_weights', 'pid'].sum())
-
-    # rename the summed energy depositions for each step within the event to "energy". This is analogous to the event energy you'd see in your detector
-	procdf = procdf.rename(columns={'Edep':'energy'})
-
-
-	procdf['x'] = procdf['x_weights']/procdf['energy']
-	procdf['y'] = procdf['y_weights']/procdf['energy']
-	procdf['z'] = procdf['z_weights']/procdf['energy']
-
-	del procdf['x_weights']
-	del procdf['y_weights']
-	del procdf['z_weights']
-
-    return procdf
+        # print(g4sdf)
+        # xarr = np.array(g4sdf['volID'])
+        # print(xarr[:])
+        # print(type(g4sntuple['x']['pages']))
+        # exit()
 
 
 if __name__ == '__main__':
