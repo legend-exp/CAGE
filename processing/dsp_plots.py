3944c2fe3efb4b0f03b1f5eb89364eb9aa2d7fd5
gothman <gulden.othman@gmail.com>
2020-10-27 15:47:22 -0700

    updates to dsp_plots to include DCR

--- processing/dsp_plots.py
+++ processing/dsp_plots.py
@@ -25,9 +25,15 @@ def main():
     plot_energy(runs)
 
 def plot_energy(runs):
-    radius_arr = []
-    mean_energy_arr = []
-    count_arr = []
+    radius_arr_1 = []
+    mean_energy_arr_1 = []
+    mean_dcr_arr_1 = []
+    count_arr_1 = []
+
+    radius_arr_2 = []
+    mean_energy_arr_2 = []
+    mean_dcr_arr_2 = []
+    count_arr_2 = []
 
 
     for run in runs:
@@ -50,15 +56,18 @@ def plot_energy(runs):
             scan_pos = alphaDB.loc[alphaDB['run']==run]
             radius = np.array(scan_pos['radius'])[0]
             angle = np.array(scan_pos['angle'])[0]
+            angle_det = 270 + angle
             print(f'Radius: {radius}; Angle: {angle}')
+
         else:
-            radius = 'bkg'
-            angle = 'bkg'
+            radius = 'n/a'
+            angle = 'n/a'
+            angle_det = 'n/a'
 
         # get hit df
         lh5_dir = dg.lh5_user_dir #if user else dg.lh5_dir
         hit_list = lh5_dir + dg.file_keys['hit_path'] + '/' + dg.file_keys['hit_file']
-        df_hit = lh5.load_dfs(hit_list, ['trapEmax', 'bl','bl_sig','A_10','AoE', 'ts_sec', 'dcr_raw', 'dcr_ftp', 'dcr_max', 'tp_10', 'tp_90', 'tp_50', 'tp_80', 'tp_max'], 'ORSIS3302DecoderForEnergy/hit')
+        df_hit = lh5.load_dfs(hit_list, ['trapEmax', 'bl','bl_sig','A_10','AoE', 'ts_sec', 'dcr_raw', 'dcr_ftp', 'dcr_max', 'tp_0', 'tp_10', 'tp_90', 'tp_50', 'tp_80', 'tp_max'], 'ORSIS3302DecoderForEnergy/hit')
 
         # use baseline cut
         df_cut = df_hit.query('bl > 8500 and bl < 10000').copy()
@@ -67,33 +76,66 @@ def plot_energy(runs):
         const = 0.0555
         df_cut['dcr_linoff'] = df_cut['dcr_raw'] + const*df_cut['trapEmax']
 
+        #create 0-50
+        df_cut['tp0_50'] = df_cut['tp_50']- df_cut['tp_0']
+
         # create cut for alphas
-        alpha_cut = 'trapEmax> 2000 and trapEmax < 12000 and dcr_linoff > 30 and dcr_linoff < 200 and AoE > 0.03 and AoE < 0.06'
+        alpha_cut = 'dcr_linoff > 25 and dcr_linoff < 200 and tp0_50 > 100 and tp0_50 < 400 and trapEmax < 12000'
         new_dcr_cut = df_cut.query(alpha_cut).copy()
         # len(new_dcr_cut)
 
         alpha_energy = np.array(new_dcr_cut['trapEmax'])
         mean_energy = np.mean(alpha_energy)
 
-        radius_arr.append(radius)
-        mean_energy_arr.append(mean_energy)
-        count_arr.append(len(alpha_energy))
+        alpha_dcr = np.array(new_dcr_cut['dcr_linoff'])
+        mean_dcr = np.mean(alpha_dcr)
 
-    energy_plot = plt.plot(radius_arr, mean_energy_arr, '.r')
+        if radius%5 == 0:
+            radius_arr_1.append(radius)
+            mean_energy_arr_1.append(mean_energy)
+            mean_dcr_arr_1.append(mean_dcr)
+            count_arr_1.append(len(alpha_energy))
+
+        else:
+            radius_arr_2.append(radius)
+            mean_energy_arr_2.append(mean_energy)
+            mean_dcr_arr_2.append(mean_dcr)
+            count_arr_2.append(len(alpha_energy))
+
+    energy_plot = plt.plot(radius_arr_1, mean_energy_arr_1, '.r', label='Scan 1')
     plt.xlabel('Radial position (mm)')
     plt.ylabel('Mean energy (trapEmax; uncal)')
 #     plt.yscale('log')
     plt.title('Mean energy of alphas by radial position; normal incidence')
+
+
+    plt.plot(radius_arr_2, mean_energy_arr_2, '.b', label='Scan 2')
+    plt.legend()
+
     plt.savefig('./plots/normScan/energy_deg.png', dpi=200)
-    
+
     plt.clf()
 
-    rate_plot = plt.plot(radius_arr, count_arr, '.r')
+    dcr_plot = plt.plot(radius_arr_1, mean_dcr_arr_1, '.r', label='Scan 1')
     plt.xlabel('Radial position (mm)')
-    plt.ylabel('Total counts)')
-#     plt.yscale('log')
-    plt.title('Alpha counts by radial position (based on DCR cut)')
-    plt.savefig('./plots/normScan/counts_alpha.png', dpi=200)
+    plt.ylabel('Mean energy (trapEmax; uncal)')
+    #    plt.yscale('log')
+    plt.title('Mean DCR value by radial position; normal incidence')
+
+
+    plt.plot(radius_arr_2, mean_dcr_arr_2, '.b', label='Scan 2')
+    plt.legend()
+
+    plt.savefig('./plots/normScan/dcr_avg.png', dpi=200)
+
+    # plt.clf()
+
+#     rate_plot = plt.plot(radius_arr, count_arr, '.r')
+#     plt.xlabel('Radial position (mm)')
+#     plt.ylabel('Total counts)')
+# #     plt.yscale('log')
+#     plt.title('Alpha counts by radial position (based on DCR cut)')
+#     plt.savefig('./plots/normScan/counts_alpha.png', dpi=200)
 #     print(len(count_arr), len(radius_arr))
 
 
