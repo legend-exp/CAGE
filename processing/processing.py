071a5fb529827542b26a985b3de7471b951ee380
wisecg <wisecg.neontetra@gmail.com>
2020-12-15 16:49:30 -0800

    mjcenpa updates

--- processing/processing.py
+++ processing/processing.py
@@ -50,11 +50,12 @@ def main():
     else:
         dg.file_keys = dg.file_keys[-1:]
 
-    view_cols = ['run','cycle','daq_file','runtype','startTime']#,'threshold',
-                 # 'stopTime','runtime']
-    print(dg.file_keys[view_cols].to_string())
+    # if you need a different view, i suggest modifying setup.py::show_fileDB
+    view_cols = ['run','cycle','daq_file','runtype']
+    # print(dg.file_keys[view_cols].to_string())
+    print(dg.file_keys[view_cols])
     print('Files:', len(dg.file_keys))
-#     exit()
+    # exit()
 
     # -- set options --
     nwfs = args.nwfs[0] if args.nwfs is not None else np.inf
@@ -81,7 +82,7 @@ def d2r(dg, overwrite=False, nwfs=None, vrb=False, user=False):
     """
     # print(dg.file_keys)
     # print(dg.file_keys.columns)
-    
+
 
     subs = dg.subsystems # can be blank: ['']
     # subs = ['geds'] # TODO: ignore other datastreams
@@ -101,7 +102,7 @@ def d2r(dg, overwrite=False, nwfs=None, vrb=False, user=False):
         if not overwrite and os.path.exists(f_raw):
             print('file exists, overwrite not set, skipping f_raw:\n   ', f_raw)
             continue
-        
+
         cyc = row['cycle']
         if row.skip:
             print(f'Cycle {cyc} has been marked junk, will not process.')
@@ -135,12 +136,12 @@ def r2d(dg, overwrite=False, nwfs=None, vrb=False, user=False):
         if not overwrite and os.path.exists(f_dsp):
             print('file exists, overwrite not set, skipping f_dsp:\n   ', f_dsp)
             continue
-        
+
         cyc = row['cycle']
         if row.skip:
             print(f'Cycle {cyc} has been marked junk, will not process.')
             continue
-        
+
         print(f'Processing cycle {cyc}')
         raw_to_dsp(f_raw, f_dsp, dsp_config, n_max=nwfs, verbose=vrb,
                    overwrite=overwrite)
@@ -179,7 +180,7 @@ def d2h(dg, overwrite=False, nwfs=None, vrb=False, user=False):
 
         # if you want to use a dsp file from the main $CAGE_LH5 directory
         f_dsp = f"{dg.lh5_dir}/{row['dsp_path']}/{row['dsp_file']}"
-        
+
         #if you want to use a dsp file from $CAGE_LH5_USER directory
         # f_dsp = f"{lh5_dir}/{row['dsp_path']}/{row['dsp_file']}"
         f_hit = f"{lh5_dir}/{row['hit_path']}/{row['hit_file']}"
@@ -187,7 +188,7 @@ def d2h(dg, overwrite=False, nwfs=None, vrb=False, user=False):
         if not overwrite and os.path.exists(f_hit):
             print('file exists, overwrite not set, skipping f_hit:\n   ', f_dsp)
             continue
-        
+
         cyc = row['cycle']
         if row.skip:
             print(f'Cycle {cyc} has been marked junk, will not process.')
@@ -206,18 +207,18 @@ def dsp_to_hit_cage(f_dsp, f_hit, dg, n_max=None, verbose=False, t_start=None):
     - timestamp correction
     for a more general dsp_to_hit, maybe each function could be given in terms
     of an 'apply' on a dsp dataframe ...
-    
-    TODO: create entry config['rawe'] with list of energy pars to calibrate, as 
+
+    TODO: create entry config['rawe'] with list of energy pars to calibrate, as
     in energy_cal.py
     """
     rawe = ['trapEmax']
-    
+
     # create initial 'hit' DataFrame from dsp data
     hit_store = lh5.Store()
     data, n_rows = hit_store.read_object(dg.config['input_table'], f_dsp)
     df_hit = data.get_dataframe()
-    
-    # 1. get energy calibration for this run from peakfit 
+
+    # 1. get energy calibration for this run from peakfit
     cal_db = db.TinyDB(storage=MemoryStorage)
     with open(dg.config['ecaldb']) as f:
         raw_db = json.load(f)
@@ -257,25 +258,25 @@ def dsp_to_hit_cage(f_dsp, f_hit, dg, n_max=None, verbose=False, t_start=None):
         ts_new.append(ts_block + t_roll)
         t_roll += t_last + t_diff
     df_hit['ts_sec'] = np.concatenate(ts_new)
-    
+
     # 3. compute global timestamp
     if t_start is not None:
-        df_hit['ts_glo'] = df_hit['ts_sec'] + t_start 
-    
+        df_hit['ts_glo'] = df_hit['ts_sec'] + t_start
+
     # write to LH5 file
     if os.path.exists(f_hit):
         os.remove(f_hit)
     sto = lh5.Store()
     tb_name = dg.config['input_table'].replace('dsp', 'hit')
     tb_lh5 = lh5.Table(size=len(df_hit))
-    
+
     for col in df_hit.columns:
         tb_lh5.add_field(col, lh5.Array(df_hit[col].values, attrs={'units':''}))
         print(col)
-    
+
     print(f'Writing table: {tb_name} in file:\n   {f_hit}')
     sto.write_object(tb_lh5, tb_name, f_hit)
-    
+
 
 def uncal_dsp_to_hit_cage(f_dsp, f_hit, dg, n_max=None, verbose=False, t_start=None):
     """
@@ -285,18 +286,18 @@ def uncal_dsp_to_hit_cage(f_dsp, f_hit, dg, n_max=None, verbose=False, t_start=N
     - timestamp correction
     for a more general dsp_to_hit, maybe each function could be given in terms
     of an 'apply' on a dsp dataframe ...
-    
-    TODO: create entry config['rawe'] with list of energy pars to calibrate, as 
+
+    TODO: create entry config['rawe'] with list of energy pars to calibrate, as
     in energy_cal.py
     """
     rawe = ['trapEmax']
-    
+
     # create initial 'hit' DataFrame from dsp data
     hit_store = lh5.Store()
     data, n_rows = hit_store.read_object(dg.config['input_table'], f_dsp)
     df_hit = data.get_dataframe()
-    
-#     # 1. get energy calibration for this run from peakfit 
+
+#     # 1. get energy calibration for this run from peakfit
 #     cal_db = db.TinyDB(storage=MemoryStorage)
 #     with open(dg.config['ecaldb']) as f:
 #         raw_db = json.load(f)
@@ -334,22 +335,22 @@ def uncal_dsp_to_hit_cage(f_dsp, f_hit, dg, n_max=None, verbose=False, t_start=N
         ts_new.append(ts_block + t_roll)
         t_roll += t_last + t_diff
     df_hit['ts_sec'] = np.concatenate(ts_new)
-    
+
     # 3. compute global timestamp
     if t_start is not None:
-        df_hit['ts_glo'] = df_hit['ts_sec'] + t_start 
-    
+        df_hit['ts_glo'] = df_hit['ts_sec'] + t_start
+
     # write to LH5 file
     if os.path.exists(f_hit):
         os.remove(f_hit)
     sto = lh5.Store()
     tb_name = dg.config['input_table'].replace('dsp', 'hit')
     tb_lh5 = lh5.Table(size=len(df_hit))
-    
+
     for col in df_hit.columns:
         tb_lh5.add_field(col, lh5.Array(df_hit[col].values, attrs={'units':''}))
         print(col)
-    
+
     print(f'Writing table: {tb_name} in file:\n   {f_hit}')
     sto.write_object(tb_lh5, tb_name, f_hit)
 
