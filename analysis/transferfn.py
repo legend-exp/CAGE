ce05114f007580fc70e9aa22c05b6aab281ae7e6
wisecg <wisecg.neontetra@gmail.com>
2020-12-18 15:41:22 -0800

    save superpulses to file

--- analysis/transferfn.py
+++ analysis/transferfn.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import os
+import time
 import numpy as np
 import pandas as pd
 from io import StringIO
 from scipy.optimize import curve_fit
+from scipy.stats import mode
 import matplotlib.pyplot as plt
 plt.style.use('../clint.mpl')
 
@@ -12,9 +14,16 @@ import pygama.io.lh5 as lh5
 import pygama.analysis.histograms as pgh
 import pygama.analysis.peak_fitting as pgf
 
+import warnings
+warnings.filterwarnings('ignore', category=pd.io.pytables.PerformanceWarning)
+
 def main():
     """
     """
+    # set output file
+    # f_super = './data/superpulses_oct2020.h5' # todo
+    f_super = './data/superpulses_dec2020.h5'
+
     # Set 1 (Oct 2020)
     # https://elog.legend-exp.org/UWScanner/249 (one file per run)
     tb1 = StringIO("""
@@ -66,7 +75,8 @@ def main():
     # -- run routines --
     # show_gain(dfp1, dfp2)
     # show_spectra(dfp2, dg)
-    get_superpulses(dfp2, dg)
+    get_superpulses(dfp2, dg, f_super)
+    get_transferfn(f_super)
 
 
 def show_gain(dfp1, dfp2):
@@ -106,6 +116,7 @@ def show_gain(dfp1, dfp2):
     plt.legend()
     # plt.show()
     plt.savefig('./plots/pulser_energyrough.pdf')
+    plt.cla()
 
 
 def show_spectra(dfp, dg):
@@ -159,8 +170,8 @@ def show_spectra(dfp, dg):
         plt.axvline(epk, lw=1, alpha=0.5)
 
         # load pulser data
-        f_cyc = dg.lh5_dir + '/' + df_row.dsp_path + '/' + df_row.dsp_file
-        pdata = lh5.load_nda([f_cyc], ['energy'], tb_name)['energy'] * ecal
+        f_dsp = dg.lh5_dir + '/' + df_row.dsp_path + '/' + df_row.dsp_file
+        pdata = lh5.load_nda([f_dsp], ['energy'], tb_name)['energy'] * ecal
 
         # take a wide window around where we expect the pulser peak
         pdata = pdata[(pdata > epk-50) & (pdata < epk+50)]
@@ -175,39 +186,56 @@ def show_spectra(dfp, dg):
     plt.xlabel(f'onboard energy (keV, c={ecal:.2e})', ha='right', x=1)
     plt.ylabel('cts / s', ha='right', y=1)
     plt.legend(fontsize=10)
+    plt.savefig('./plots/transferfn_peaks.pdf')
     plt.show()
-    # plt.savefig('./plots/transferfn_peaks.pdf')
+    plt.clf()
 
 
-def get_superpulses(dfp, dg):
+def get_superpulses(dfp, dg, f_super):
     """
-    calculate average waveforms for each set of pulser data
+    calculate average waveforms for each set of pulser data.
+    save an output file with the superpulses for further analysis.
     """
-    # find this with show_spectra
+    # find this with the show_spectra function above
+    # ecal = 1460.8 / 2.005e6 # TODO: find the const for oct 2020
     ecal = 1460.8 / 2.005e6 # works for pulser dataset 2 (dec 2020)
 
-    tb_name = 'ORSIS3302DecoderForEnergy/dsp'
+    # more settings
+    show_plots = True # default True
+    write_output = True
+    nwfs = 1000 # limit number to go fast.  1000 is enough for a good measurement
+    tp_align = 0.5 # pct timepoint to align wfs at
+    e_window = 10 # plot (in keV) this window around each pulser peak
+    n_pre, n_post = 50, 100 # num samples before/after tp_align
+    bl_thresh = 10 # allowable baseline ADC deviation
 
-    for i, df_row in dfp.iterrows():
+    dsp_name = 'ORSIS3302DecoderForEnergy/dsp'
+    raw_name = 'ORSIS3302DecoderForEnergy/raw/waveform'
 
-        epk, rt, vp = df_row[['E_keV', 'runtime', 'V_pulser']]
+    sto = lh5.Store()
+    t_start = time.time()
+
+    def analyze_pulser_run(df_row):
+        """
+        loop over each row of dfp and save the superpulse
+        """
+        epk, rt, vp, cyc = df_row[['E_keV', 'runtime', 'V_pulser','cycle']]
         rt *= 60 # sec
-        if epk == 0: continue # skip the bkg run
+        if epk == 0: return [] # skip the bkg run
 
         # load pulser energies
-        f_cyc = dg.lh5_dir + '/' + df_row.dsp_path + '/' + df_row.dsp_file
+        f_dsp = dg.lh5_dir + '/' + df_row.dsp_path + '/' + df_row.dsp_file
+        pdata = lh5.load_nda([f_dsp], ['energy'], dsp_name)['energy'] * ecal
 
-        pdata = lh5.load_nda([f_cyc], ['energy'], tb_name)['energy'] * ecal
-
-        # take a more narrow window around the max pulser peak
+        # auto-narrow the window around the max pulser peak in two steps
         elo, ehi, epb = epk-50, epk+50, 0.5
         pdata_all = pdata[(pdata > elo) & (pdata < ehi)]
         hp, bp, _ = pgh.get_hist(pdata_all, range=(elo, ehi), dx=epb)
         pctr = bp[np.argmax(hp)]
 
-        plo, phi, ppb = pctr-10, pctr+10, 0.1
-        pdata = pdata[(pdata > plo) & (pdata < phi)]
-        hp, bp, _ = pgh.get_hist(pdata_all, range=(plo, phi), dx=ppb)
+        plo, phi, ppb = pctr - e_window, pctr + e_window, 0.1
+        pdata_pk = pdata[(pdata > plo) & (pdata < phi)]
+        hp, bp, _ = pgh.get_hist(pdata_pk, range=(plo, phi), dx=ppb)
         hp_rt = np.divide(hp, rt)
         hp_var = np.array([np.sqrt(h / (rt)) for h in hp])
 
@@ -221,27 +249,121 @@ def get_superpulses(dfp, dg):
         fwhm = upr_half - bot_half
         sig0 = fwhm / 2.355
         amp0 = np.amax(hp_rt) * fwhm
-        p_init = [amp0, bp[imax], sig0, bkg0] # a, mu, sigma, bkg
+        p_init = [amp0, bp[imax], sig0, bkg0]
         p_fit, p_cov = pgf.fit_hist(pgf.gauss_bkg, hp_rt, bp,
                                     var=hp_var, guess=p_init)
-        fit_func = pgf.gauss_bkg
-        xfit = np.arange(plo, phi, ppb * 0.1)
-        plt.plot(xfit, fit_func(xfit, *p_init), '-', c='orange',
-                 label='init')
-        plt.plot(xfit, fit_func(xfit, *p_fit), '-', c='red',
-                 label='fit')
+        amp, mu, sigma, bkg = p_fit
 
-        # plot data
-        plt.plot(bp[1:], hp_rt, ds='steps', c='k', lw=1, label=f'{vp:.2f} V')
+        # select events within 1 sigma of the maximum
+        # and pull the waveforms from the raw file to make a superpulse.
+        idx = np.where((pdata >= mu-sigma) & (pdata <= mu+sigma))
+        print(f'Pulser at {epk} keV, {len(idx[0])} events.  Limiting to {nwfs}.')
+        if len(idx[0]) > nwfs:
+            idx = idx[0][:nwfs]
 
-        plt.xlabel(f'onboard energy (keV, c={ecal:.2e})', ha='right', x=1)
-        plt.ylabel('cts / s', ha='right', y=1)
-        plt.legend(fontsize=10)
-        plt.show()
+        # grab the 2d numpy array of pulser wfs
+        n_rows = idx[-1]+1 # read up to this event and stop
+        f_raw = dg.lh5_dir + '/' + df_row.raw_path + '/' + df_row.raw_file
+        tb_wfs, n_wfs = sto.read_object(raw_name, f_raw, n_rows=n_rows)
+        pwfs = tb_wfs['values'].nda[idx,:]
+        # print(idx, len(idx), pwfs.shape, '\n', pwfs)
 
-        # now I want to select events within 1 sigma of the maximum
-        # and pull all the waveforms to make a superpulse.
-        print('lol tbd')
+        # data cleaning step: remove events with outlier baselines
+        bl_means = pwfs[:,:500].mean(axis=1)
+        bl_mode = mode(bl_means.astype(int))[0][0]
+        bl_ctr = np.subtract(bl_means, bl_mode)
+        idx_dc = np.where(np.abs(bl_ctr) < bl_thresh)
+        pwfs = pwfs[idx_dc[0],:]
+        bl_means = bl_means[idx_dc]
+        # print(pwfs.shape, bl_means.shape)
+
+        # baseline subtract (trp when leading (not trailing) dim is the same)
+        wfs = (pwfs.transpose() - bl_means).transpose()
+
+        # time-align all wfs at their 50% timepoint (tricky!).
+        # adapted from pygama/sandbox/old_dsp/[calculators,transforms].py
+        # an alternate approach would be to use ProcessingChain here
+        wf_maxes = np.amax(wfs, axis=1)
+        timepoints = np.argmax(wfs >= wf_maxes[:, None]*tp_align, axis=1)
+        wf_idxs = np.zeros([wfs.shape[0], n_pre + n_post], dtype=int)
+        row_idxs = np.zeros_like(wf_idxs)
+        for i, tp in enumerate(timepoints):
+            wf_idxs[i, :] = np.arange(tp - n_pre, tp + n_post)
+            row_idxs[i, :] = i
+        wfs = wfs[row_idxs, wf_idxs]
+
+        # take the average to get the superpulse
+        superpulse = np.mean(wfs, axis=0)
+
+        # normalize all wfs to the superpulse maximum
+        wfmax, tmax = np.amax(superpulse), np.argmax(superpulse)
+        superpulse = np.divide(superpulse, wfmax)
+        wfs = np.divide(wfs, wfmax)
+
+        # -- plot results --
+        if show_plots:
+            fig, (p0, p1) = plt.subplots(2, figsize=(7,8))
+
+            # plot fit result (top), and waveforms + superpulse (bottom)
+            xfit = np.arange(plo, phi, ppb * 0.1)
+            p0.plot(xfit, pgf.gauss_bkg(xfit, *p_init), '-', c='orange',
+                    label='init')
+            p0.plot(xfit, pgf.gauss_bkg(xfit, *p_fit), '-', c='red', label='fit')
+
+            # plot 1 sigma window
+            p0.axvspan(mu-sigma, mu+sigma, color='m', alpha=0.2, label='1 sigma')
+
+            # plot data
+            p0.plot(bp[1:], hp_rt, ds='steps', c='k', lw=1, label=f'{vp:.2f} V')
+            p0.set_xlabel(f'onboard energy (keV, c={ecal:.2e})', ha='right', x=1)
+            p0.set_ylabel('cts / s', ha='right', y=1)
+            p0.legend(fontsize=10)
+
+            # plot individ. wfs
+            ts = np.arange(0, len(wfs[0,:]))
+            for iwf in range(wfs.shape[0]):
+                p1.plot(ts, wfs[iwf,:], '-k', lw=2, alpha=0.5)
+            p1.plot(np.nan, np.nan, '-k', label=f'wfs, {epk:.0f} keV')
+
+            # plot superpulse
+            p1.plot(ts, superpulse, '-r', lw=2, label=f'superpulse, {vp:.2f} V')
+
+            p1.set_xlabel('time (10 ns)', ha='right', x=1)
+            p1.set_ylabel('amplitude', ha='right', y=1)
+            p1.legend(fontsize=10)
+            # plt.show()
+            plt.savefig(f'./plots/superpulse_cyc{cyc}.png', dpi=150)
+            plt.cla()
+
+        # save the superpulse to our output file
+        return superpulse
+
+    dfp['superpulse'] = dfp.apply(analyze_pulser_run, axis=1)
+
+    # drop the duplicated 'run' row before saving
+    dfp = dfp.loc[:,~dfp.columns.duplicated()]
+    # print(dfp.columns)
+    print(dfp)
+
+    if write_output:
+        print('Saving output file: ', f_super)
+        dfp.to_hdf(f_super, key='superpulses')
+
+    t_elap = (time.time() - t_start) / 60
+    print(f'Done.  Elapsed: {t_elap:.2f} min.')
+
+
+def get_transferfn(f_super):
+    """
+    for each energy, integrate the superpulse and compute the transfer function
+    """
+    dfp = pd.read_hdf(f_super, key='superpulses')
+    print(dfp)
+
+    def measure_tf(df_row):
+        print(df_row)
+
+    res = dfp.apply(measure_tf, axis=1)
 
 
 if __name__=="__main__":
