b35ce976f4469f90b06e85ac3f18c1776fdb5483
Gulden Othman <gulden.othman@gmail.com>
2021-01-14 13:21:39 -0500

    minor changes trying to fix formatting errors causing problems on cori

--- sims/post_process/rates.py
+++ sims/post_process/rates.py
@@ -26,38 +26,38 @@ def main():
     # getRate(processed_filename, primaries, elo, ehi) # get rate in counts/sec for specific energy region
     # plotRate(radius, elo, ehi) # plot rates for multiple source positions (sims files) on one plot
 
-
 def getCounts(processed_filename):
-	df = pd.read_hdf(processed_filename, keys='procdf')
-	energy = np.array(df['energy'])
-	counts = len(energy)
-	print('%f counts in PV' %counts)
+    df = pd.read_hdf(processed_filename, keys='procdf')
+    energy = np.array(df['energy'])
+    counts = len(energy)
+    print('%f counts in PV' %counts)
 
 def getCounts_cut(processed_filename, elo, ehi):
-	df = pd.read_hdf(processed_filename, keys='procdf')
-	energy = np.array(df['energy'])
-	cut_df = df.loc[(df.energy > elo) & (df.energy < ehi)]
-	cut_energy_keV = np.array(cut_df['energy']*1000)
-	counts = len(cut_energy_keV)
-	print(f'{counts} counts in region {elo} to {ehi} keV')
+    df = pd.read_hdf(processed_filename, keys='procdf')
+    energy = np.array(df['energy'])
+    cut_df = df.loc[(df.energy > elo) & (df.energy < ehi)]
+    cut_energy_keV = np.array(cut_df['energy']*1000)
+    counts = len(cut_energy_keV)
+    print(f'{counts} counts in region {elo} to {ehi} keV')
 
-	return(counts)
+    return(counts)
 
 def getRate(processed_filename, primaries, elo, ehi):
-	# see this elog https://elog.legend-exp.org/UWScanner/166
-	source_activity = 4.0e4 #40 kBq = 4e4 decays/s
-	time_seconds = primaries/(source_activity)
-	counts = getCounts_cut(processed_filename, elo, ehi)
-	rate = counts/time_seconds #(rate in counts/s)
-	print(f'{rate} counts/second in region {elo} to {ehi} keV')
+    # see this elog https://elog.legend-exp.org/UWScanner/166
+    source_activity = 4.0e4 #40 kBq = 4e4 decays/s
+    time_seconds = primaries/(source_activity)
+    counts = getCounts_cut(processed_filename, elo, ehi)
+    rate = counts/time_seconds #(rate in counts/s)
+    print(f'{rate} counts/second in region {elo} to {ehi} keV')
 
 def plotRate(radius, elo, ehi):
-	rates_arr = []
-	for r in radius:
-		rate = getRate(f'../alpha/processed_out/oppi/processed_oppi_ring_y{r}_norm_241Am_100000000.hdf5', 10000000, 5.4, 5.6)
-		rates_arr.append(rate)
-	plt.plot(radius, rates_arr, '.r')
-	plt.savefig('./rates.png')
+    rates_arr = []
+    for r in radius:
+        rate = getRate(f'../alpha/processed_out/oppi/processed_oppi_ring_y{r}_norm_241Am_100000000.hdf5', 10000000, 5.4, 5.6)
+        rates_arr.append(rate)
+        
+    plt.plot(radius, rates_arr, '.r')
+    plt.savefig('./rates.png')
     #return(rate)
 
 if __name__ == '__main__':
