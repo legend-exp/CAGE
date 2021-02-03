559d245d435f2a400a432212ea1c63de1fd6ca38
Gulden Othman <gulden.othman@gmail.com>
2021-02-03 12:11:25 -0500

    updates to gif_plots.py to create gifs for angle scans

--- analysis/gif_plots.py
+++ analysis/gif_plots.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 import imageio
+import glob
 
 
 def main():
 
-    runs = [64, 44, 66, 48, 70, 50, 72, 54, 60, 42] #starting at 7.5mm so if doesn't show up in ppt at least will show a plot with alphas
-    plot_dir = './plots/normScan/cal_normScan/'
+    # runs = [64, 44, 66, 48, 70, 50, 72, 54, 60, 42] #starting at 7.5mm so if doesn't show up in ppt at least will show a plot with alphas
+    # plot_dir = './plots/normScan/cal_normScan/'
 
-    dsp_params = ['alp_energy_run', 'alp_AoE_run', 'alp_dcr_run', 'alp_AoE_vs_dcr_run', 'alp_dcr_vs_tp0_50_run', 'alp_1d_aoe_run',
-                'alp_cut_energy_run', 'alp_cut_AoE_run', 'alp_cut_dcr_run', 'alp_cut_AoE_vs_dcr_run', 'alp_cut_dcr_vs_tp0_50_run', 'alp_cut__1d_aoe_run']
-    outputs_files = ['energy.gif', 'aoeVsE.gif', 'dcrVsE.gif', 'aoeVsdcr.gif', 'dcrVstp0_50.gif', '1d_aoe.gif',
-                'cut_energy.gif', 'cut_aoeVsE.gif', 'cut_dcrVsE.gif', 'cut_aoeVsdcr.gif', 'cut_dcrVstp0_50.gif', 'cut_1d_aoe.gif']
+    plot_dir = './plots/angleScan/'
+    radii = ['18']
 
-    dsp_gif(dsp_params, runs, plot_dir, outputs_files)
+
+    dsp_params = ['alp_energy_', 'alp_AoE_', 'alp_DCR_', 'alp_AoEvDCR_', 'alp_DCRvTp050_']
+    outputs_files = ['energy.gif', 'aoeVsE.gif', 'dcrVsE.gif', 'aoeVsdcr.gif', 'dcrVstp0_50.gif']
+
+    # dsp_params = ['alp_energy_run', 'alp_AoE_run', 'alp_dcr_run', 'alp_AoE_vs_dcr_run', 'alp_dcr_vs_tp0_50_run', 'alp_1d_aoe_run',
+                # 'alp_cut_energy_run', 'alp_cut_AoE_run', 'alp_cut_dcr_run', 'alp_cut_AoE_vs_dcr_run', 'alp_cut_dcr_vs_tp0_50_run', 'alp_cut__1d_aoe_run']
+    # outputs_files = ['energy.gif', 'aoeVsE.gif', 'dcrVsE.gif', 'aoeVsdcr.gif', 'dcrVstp0_50.gif', '1d_aoe.gif',
+                # 'cut_energy.gif', 'cut_aoeVsE.gif', 'cut_dcrVsE.gif', 'cut_aoeVsdcr.gif', 'cut_dcrVstp0_50.gif', 'cut_1d_aoe.gif']
+
+    # dsp_gif(dsp_params, runs, plot_dir, outputs_files)
+    dsp_gif_byRadius(dsp_params, radii, plot_dir, outputs_files)
 
 def dsp_gif(dsp_params, runs, plot_dir, outputs_files):
 
@@ -23,6 +32,19 @@ def dsp_gif(dsp_params, runs, plot_dir, outputs_files):
 
         imageio.mimsave(f'{plot_dir}{outfile}', images, fps=3)
         print('Saving ', f'{plot_dir}{outfile}')
+
+def dsp_gif_byRadius(dsp_params, radii, plot_dir, outputs_files):
+
+    for param, outfile in zip(dsp_params, outputs_files):
+        for radius in radii:
+            images = []
+            # print(sorted(glob.glob(f'{plot_dir}{param}{radius}*.png')))
+            for file in sorted(glob.glob(f'{plot_dir}{param}{radius}*.png'), reverse=True):
+                print(radius, file)
+                images.append(imageio.imread(file))
+
+            imageio.mimsave(f'{plot_dir}{radius}mm_{outfile}', images, fps=1)
+            print('Saving ', f'{plot_dir}{radius}mm_{outfile}')
 
 
 if __name__=="__main__":
