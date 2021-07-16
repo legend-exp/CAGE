00b542c19c18b65702548db381e106390b52edd4
wisecg <wisecg.neontetra@gmail.com>
2021-07-16 10:57:08 -0700

    update runDB

--- processing/cagejob.sh
+++ processing/cagejob.sh
@@ -29,7 +29,8 @@ fi
 # shifter python setup.py --update --orca -b
 
 # -- Campaign 2 workspace --
-shifter python processing.py -q 'run >= 242' --d2r --r2d
+shifter python processing.py -q 'run >= 245 and run <= 250' --d2r --r2d
+shifter python setup.py --orca --rt -b
 
 # shifter python processing.py -q 'run >= 235' --d2r -o
 # shifter python processing.py -q 'run >= 235' --r2d -o
