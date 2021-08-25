bd53b4a45ed28a455b46be62a2020de0180d6fa2
Grace Song <grsong@uw.edu>
2021-08-25 12:20:24 -0700

    prep_sims can write jobsub

--- new_sims/prep_sims.py
+++ new_sims/prep_sims.py
@@ -18,23 +18,24 @@ def main():
 
     G. Othman
     """
-    radius = [15.]
-    source_angle = [90.]
+    radius = [10, 15.]
+    source_angle = [45.]
     rotary = [0.]
 #     rotary = np.linspace(4, 144, 15)
     # print(rotary)
     # exit()
     mac_dir = './macros/'
     gdml_dir = './geometries/mothers/'
-    hdf5_dir = '$TMPDIR/'
+    hdf5_dir = '$[TMPDIR]/' #enclose environment variables in square brackets [] 
     run = 'centering_scan/' #ex 'centering_scan/'
     det = 'oppi'
-    primaries = 100
+    primaries = 10
+    jobs = 1
     # print(f'./geometries/mothers/{det}/{run}test.gdml')
-    writeFiles(radius, source_angle, rotary, det, run, primaries, hdf5_dir=hdf5_dir, write_shell=False, run_job=False)
+    writeFiles(radius, source_angle, rotary, det, run, primaries, jobs, hdf5_dir=hdf5_dir, write_shell=True, run_job=True)
 
 
-def writeFiles(radius, source_angle, rotary='0', det='oppi', run = '', primaries=100000000, mac_dir = './macros/', gdml_dir = './geometries/mothers/', hdf5_dir = './alpha/raw_out/', write_shell=False, run_job=False):
+def writeFiles(radius, source_angle, rotary='0', det='oppi', run = '', primaries=100000000, jobs=10, mac_dir = './macros/', gdml_dir = './geometries/mothers/', hdf5_dir = './alpha/raw_out/', write_shell=False, run_job=False):
     batch_job = []
     for r in radius:
         for theta_det in source_angle:
@@ -45,9 +46,9 @@ def writeFiles(radius, source_angle, rotary='0', det='oppi', run = '', primaries
                 hdf5_out_file = hdf5_dir+ f'y{int(r)}_thetaDet{int(theta_det)}_rotary{int(theta_rot)}_241Am_{primaries}.hdf5'
 
                 #Create directory for hdf5 output if doesn't alreasy exist
-                #if not os.path.isdir(hdf5_dir +f'{det}/{run}'):
-                #    print(f'Creating directory for hdf5 output file: {gdml_dir + det +"/"+run}')
-                #    os.mkdir(hdf5_dir +f'{det}/{run}')
+                #if not os.path.isdir(hdf5_dir):
+                #    print(f'Creating directory for hdf5 output file: {hdf5_dir}')
+                #    os.mkdir(hdf5_dir)
                     
                 with open(gdml_dir + f'{det}/template.gdml', 'r') as file:
                     # read a list of lines into data
@@ -58,16 +59,16 @@ def writeFiles(radius, source_angle, rotary='0', det='oppi', run = '', primaries
                     mac = file.readlines()
 
                 mac_rotation, gdml_source_center, gdml_source_rotation = positionCalc(r, theta_det)
-                gdml_divingBoard_rotation = f'     <rotation name="OPPI1_diving_board_volume_Rotation" z="(180-17.92)-{theta_rot}" unit="deg"/> <!-- diving board rotation. linear motor drive \n'
+                gdml_divingBoard_rotation = f'     <rotation name="OPPI1_diving_board_volume_Rotation" z="(180-17.92)-{theta_rot}" unit="deg"/> <!-- diving board rotation. linear motor drive --> \n'
                 gdml_det_rotation = f'     <rotation name="OPPI_Rotation" x="0" y="0" z="-{theta_rot}" unit="deg"/> <!--Add same additional rotation to "OPPI1_diving_board_volume_Rotation" here to simulte rotating rotary motor--> \n'
 
-                gdml[89] = gdml_source_center
-                gdml[90] = gdml_source_rotation
-                gdml[102] = gdml_divingBoard_rotation
-                gdml[109] = gdml_det_rotation
+                gdml[78] = gdml_source_center
+                gdml[79] = gdml_source_rotation
+                gdml[91] = gdml_divingBoard_rotation
+                gdml[97] = gdml_det_rotation
 
                 mac[9] = '/g4simple/setDetectorGDML ' + gdml_out_file + ' \n'
-                mac[13] = '/analysis/setFileName ' + hdf5_out_file + ' \n'
+                mac[13] = '/g4simple/setFileName ' + hdf5_out_file + ' \n'
 
                 if int(theta_det)==90:
                     mac[43] = f'/gps/pos/centre 0.0 {float(r)} 4.5 mm \n'
@@ -102,24 +103,29 @@ def writeFiles(radius, source_angle, rotary='0', det='oppi', run = '', primaries
                         file.writelines(mac)
 
                 if write_shell:
-                    # cori_command = f'echo $SHELL \n' #just do this for testing
-                    cori_command = f'sbatch --chdir=/global/homes/g/gothman/projecta/joule_CAGE/sims/ --output=/global/homes/g/gothman/projecta/joule_CAGE/sims/{det}_y{r}_thetaDet{int(theta_det)}_rotary{int(theta_rot)}_241Am_cori-%j.txt --image=legendexp/legend-software:latest -C haswell --export=HDF5_USE_FILE_LOCKING=FALSE --qos=shared -t 48:00:00 slurm.slr "g4simple {mac_out_file}" \n'
-                    batch_job.append(cori_command)
+                    with open(f'./submission/run_template.sh', 'r') as file:
+                        # read a list of lines into data
+                        run = file.readlines()
+                    with open(f'./submission/submit_template.sh', 'r') as file:
+                        # read a list of lines into data
+                        sub = file.readlines()
+                    run_name = f'run_y{int(r)}_thetaDet{int(theta_det)}_rot{int(theta_rot)}.sh'
+                    sub_name = f'sub_y{int(r)}_thetaDet{int(theta_det)}_rot{int(theta_rot)}.sh'
 
-    if write_shell:
-        with open('./run_sim_jobs.sh', 'w') as file:
-            file.writelines(batch_job)
+                    run[7] = f'g4simple {mac_out_file} \n'
+                    run[9] = f'h5repack -v -f GZIP=5 y{int(r)}_thetaDet{int(theta_det)}_rotary{int(theta_rot)}_241Am_{primaries}.hdf5 ' + '${DATADIR}/' + f'y{int(r)}_thetaDet{int(theta_det)}_rotary{int(theta_rot)}_' + '${SGE_TASK_ID}.hdf5 \n'
 
-    if run_job:
-        result1 = subprocess.run(["chmod +x sim_jobs.sh"], shell=True, check=True, capture_output=True, text=True).stdout.strip("\n")
-        result = subprocess.run(["./run_sim_jobs.sh"], shell=True, check=True, capture_output=True, text=True).stdout.strip("\n")
-        print(result)
+                    sub[6] = f'#$ -N y{int(r)}_thetaDet{int(theta_det)}_rotary{int(theta_rot)} #job name \n'
+                    sub[12] = f'#$ -t 1-{jobs} #give me N identical jobs, labelled by variable SGE_TASK_ID \n'
+                    sub[16] = f'singularity exec --bind /data/eliza1/LEGEND,$TMPDIR /data/eliza1/LEGEND/sw/containers/legend-base.sif /data/eliza1/LEGEND/users/grsong/CAGE/new_sims/submissions/{run_name} \n'
 
+                    with open(f'./submission/{run_name}', 'w') as file:
+                        # read a list of lines into data
+                        file.writelines(run)
+                    with open(f'./submission/{sub_name}', 'w') as file:
+                        # read a list of lines into data
+                        file.writelines(sub)
 
-
-def writeShell():
-
-    command = f'sbatch --chdir=/global/homes/g/gothman/projecta/joule_CAGE/sims/ --output=/global/homes/g/gothman/projecta/joule_CAGE/sims/{det}_y{r}_thetaDet{int(theta_det)}_rotary{int(theta_rot)}_241Am_cori-%j.txt --image=legendexp/legend-software:latest -C haswell --export=HDF5_USE_FILE_LOCKING=FALSE --qos=shared -t 48:00:00 slurm.slr "g4simple {mac_out_file}"'
 
 
 if __name__ == '__main__':
